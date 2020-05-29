c4aba8267ded7027ecf89d094e4cbaa9bc0b9415
enhao25 <larloller@gmail.com>
2020-05-29 19:56:44 +0800

    Updated homepage with styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './Component/HomePage'
+import HomePage from './Pages/HomePage/HomePage'
 
 function App() {
   return (
---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.scss'
+
+import Directory from '../../Component/Directory/Directory'
+
+const HomePage = () => (
+    <div className="homepage">
+        < Directory />
+    </div>
+)
+
+export default HomePage;
