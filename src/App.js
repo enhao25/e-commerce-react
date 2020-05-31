db7887422c77c4ac4ec24b15080adf7b7fb32d21
enhao25 <larloller@gmail.com>
2020-05-31 09:35:04 +0800

    Header Component Added

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ import {
 } from "react-router-dom";
 
 import './App.css';
-import HomePage from './Pages/HomePage/HomePage'
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+import HomePage from './Component/HomePage/HomePage';
+import ShopPage from '../src/Component/Shop/shop';
+import Header from './Component/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
