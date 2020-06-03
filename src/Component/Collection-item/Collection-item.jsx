b6a2adcea03b6996660b67ee8c62c7df59f3af3a
enhao25 <larloller@gmail.com>
2020-06-03 09:30:44 +0800

    Added Cart Item Redux

--- src/Component/Collection-item/Collection-item.jsx
+++ src/Component/Collection-item/Collection-item.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { addItem } from '../../Redux/cart/cart.actions'
 
 import './Collection-item.scss'
 import CustomButton from '../CustomButton/CustomButton';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className="collection-item">
-        <div className="image" style={{backgroundImage: `url(${imageUrl})`}} />
-        <div className="collection-footer">
-            <span className='name'>{ name }</span>
-            <span className="price">{ price }</span>
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+    return(
+        <div className="collection-item">
+            <div className="image" style={{backgroundImage: `url(${imageUrl})`}} />
+            <div className="collection-footer">
+                <span className='name'>{ name }</span>
+                <span className="price">{ price }</span>
+            </div>
+            <CustomButton onClick={() => addItem(item)} inverted> Add To Cart </CustomButton>
         </div>
-        <CustomButton inverted> Add To Cart </CustomButton>
-    </div>
-)
+)}
 
-export default CollectionItem;
+// Dispatch = Putting payload into the store / Updating the state value
+// Dispatch allow us to use the addItem function in cart.actions
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/Redux/cart/cart.actions.js
+++ src/Redux/cart/cart.actions.js
@@ -1,6 +1,12 @@
 import { CartActionTypes } from './cart.types'
 
+// Payload is optional
 export const toggleCartHidden = () => ({
     // Always align the type string to the one at the reducer switch case
     type: CartActionTypes.TOGGLE_CART_HIDDEN
+})
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
 })
