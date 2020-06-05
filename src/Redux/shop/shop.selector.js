44e8972635fd9521263ed225cf3ac1f3e4ef8590
enhao25 <larloller@gmail.com>
2020-06-05 19:56:19 +0800

    Data Normalization

--- src/Redux/shop/shop.selector.js
+++ src/Redux/shop/shop.selector.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3, 
-    womens: 4,
-    mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
@@ -18,5 +10,6 @@ export const selectShopCollections = createSelector(
 // createSelector is a curry function that is a function that returns a function
 export const selectCollection = collectionUrlParam => createSelector(
     [selectShopCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    collections =>
+        collections[collectionUrlParam]
 )
